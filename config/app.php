d3b43c1fa4ce6b39a6c5b726b6a6953c5b1a8bf0
joseph inziano <joseph inziano@WFPKENRB-FIN007.global.wfp.org>
2018-08-30 15:10:29 +0300

    Add api response builder

--- config/app.php
+++ config/app.php
@@ -150,7 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        MarcinOrlowski\ResponseBuilder\ResponseBuilderServiceProvider::class,
         /*
          * Application Service Providers...
          */
