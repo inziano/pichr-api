f1311351972ad8a83b2e85122a117b18d25782c4
joseph inziano <joseph inziano@WFPKENRB-FIN007.global.wfp.org>
2019-04-20 15:14:16 +0300

    Change default database character set

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Change default database character set
+        Schema::defaultStringLength(191);
     }
 
     /**
